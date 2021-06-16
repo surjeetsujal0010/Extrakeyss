d8bcc2ebba507546c67079fffdaec3327ed8e1dc
C.S.G <64678620+surjeetsujal0010@users.noreply.github.com>
2021-06-16 16:39:41 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,23 @@
+cd
+cd Extrakeyss
+cd .core
+cp extrakeys /data/data/com.termux/files/usr/bin
+cd 
+cd /data/data/com.termux/files/usr/bin
+chmod 777 extrakeys
+cd
+cd /data/data/com.termux/files/usr
+mkdir C.S.G
+cd
+mv Extrakeyss /data/data/com.termux/files/usr/C.S.G
+cd
+cd /data/data/com.termux/files/usr/C.S.G/Extrakeys/.core
+chmod 777 extrak.sh
+chmod 777 adex.py
+chmod 777 install.py
+cd
+clear
+
+echo -e "\e[1;91m"
+echo "***** Type (extrakeys) in termux terminal to opearate *****"
+
